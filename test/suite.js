d8fb0483b3ea7eb80b312d87c70881b3a5fa6928
Jérémy Lal <kapouer@melix.org>
2015-12-16 15:22:04 +0100

    Test youtube url

--- test/suite.js
+++ test/suite.js
@@ -3,7 +3,7 @@ var expect = require('expect.js');
 
 describe("url-inspector", function suite() {
 	it("should inspect large file without downloading it entirely", function(done) {
-		this.timeout(2000);
+		this.timeout(5000);
 		inspector('https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.3.3.tar.xz', function(err, meta) {
 			expect(err).to.not.be.ok();
 			expect(meta.type).to.be('data');
@@ -11,11 +11,20 @@ describe("url-inspector", function suite() {
 		});
 	});
 	it("should return meta with thumbnail as data-uri", function(done) {
-		this.timeout(2000);
+		this.timeout(5000);
 		inspector('https://upload.wikimedia.org/wikipedia/commons/b/bd/1110_desktop_visual.jpg', function(err, meta) {
 			expect(err).to.not.be.ok();
 			expect(meta.type).to.be('image');
 			expect(meta.thumbnail.indexOf('image/jpeg') > 0).to.be.ok();
+			done();
+		});
+	});
+	it("should return meta with thumbnail as data-uri for a youtube video", function(done) {
+		this.timeout(5000);
+		inspector('https://www.youtube.com/watch?v=CtP8VABF5pk', function(err, meta) {
+			expect(err).to.not.be.ok();
+			expect(meta.type).to.be('video');
+			expect(meta.thumbnail.indexOf('youtu') > 0).to.be.ok();
 			done();
 		});
 	});
