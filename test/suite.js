b89b06b0124ef12ba3c2f2f2dd8675b1b6f9b3ad
Jérémy Lal <kapouer@melix.org>
2016-06-01 11:51:20 +0200

    Merge two test files

--- test/suite.js
+++ test/suite.js
@@ -2,6 +2,25 @@ var inspector = require('..');
 var expect = require('expect.js');
 
 describe("url-inspector", function suite() {
+	it("should get title from http://www.lavieenbois.com/", function(done) {
+		this.timeout(5000);
+		inspector('http://www.lavieenbois.com/', function(err, meta) {
+			expect(err).to.not.be.ok();
+			expect(meta.title).to.be.ok();
+			done();
+		});
+	});
+	it("should return embeddable content at https://myspace.com/unefemmemariee/music/songs", function(done) {
+		this.timeout(5000);
+		inspector('https://myspace.com/unefemmemariee/music/songs', function(err, meta) {
+			expect(err).to.not.be.ok();
+			expect(meta.type).to.be("audio");
+			expect(meta.ext).to.be("html");
+			expect(meta.html).to.be.ok();
+			expect(meta.html.startsWith('<audio')).to.be.ok();
+			done();
+		});
+	});
 	it("should inspect large file without downloading it entirely", function(done) {
 		this.timeout(3000);
 		inspector('https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.3.3.tar.xz', function(err, meta) {
