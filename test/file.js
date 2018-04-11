3d9418494c1bd67bda644357911b6c7d0f01784c
Jérémy Lal <kapouer@melix.org>
2018-04-11 21:44:20 +0200

    Files should get size

--- test/file.js
+++ test/file.js
@@ -5,6 +5,7 @@ describe("file suite", function suite() {
 	it("should inspect relative path", function(done) {
 		inspector(`file://./test/fixtures/songs.html`, {file: true}, function(err, meta) {
 			expect(err).to.not.be.ok();
+			expect(meta.size).to.be(108482);
 			expect(meta.type).to.be("audio");
 			expect(meta.ext).to.be("html");
 			expect(meta.html).to.be.ok();
