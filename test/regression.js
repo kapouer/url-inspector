f3b13799c8a69da155abb34722fcca4ebcb6ff3c
Jérémy Lal <kapouer@melix.org>
2022-02-24 13:35:50 +0100

    Rename test

--- test/regression.js
+++ test/regression.js
@@ -1,7 +1,7 @@
 const inspector = require('..');
 const expect = require('expect.js');
 
-describe("user-agent suite", () => {
+describe("regression suite", () => {
 	it("no 403 on laprovence", async () => {
 		const meta = await inspector('https://www.laprovence.com/actu/en-direct/6620418/mercato-om-valence-pense-a-alvaro.html');
 		expect(meta.type).to.be("link");
