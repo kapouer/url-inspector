0f5e31112eccd747fbba8f7015e51d31d901a334
Jérémy Lal <kapouer@melix.org>
2022-09-13 10:00:48 +0200

    Drop instagram, now only accessible using access token

--- src/custom-oembed-providers.js
+++ src/custom-oembed-providers.js
@@ -80,44 +80,5 @@ const twitter = {
 	}]
 };
 
-const instagram = {
-	provider_name: "instagram",
-	endpoints: [{
-		"schemes": [
-			"http://instagram.com/*/p/*,",
-			"http://www.instagram.com/*/p/*,",
-			"https://instagram.com/*/p/*,",
-			"https://www.instagram.com/*/p/*,",
-			"http://instagram.com/p/*",
-			"http://instagr.am/p/*",
-			"http://www.instagram.com/p/*",
-			"http://www.instagr.am/p/*",
-			"https://instagram.com/p/*",
-			"https://instagr.am/p/*",
-			"https://www.instagram.com/p/*",
-			"https://www.instagr.am/p/*",
-			"http://instagram.com/tv/*",
-			"http://instagr.am/tv/*",
-			"http://www.instagram.com/tv/*",
-			"http://www.instagr.am/tv/*",
-			"https://instagram.com/tv/*",
-			"https://instagr.am/tv/*",
-			"https://www.instagram.com/tv/*",
-			"https://www.instagr.am/tv/*",
-			"http://www.instagram.com/reel/*",
-			"https://www.instagram.com/reel/*",
-			"http://instagram.com/reel/*",
-			"https://instagram.com/reel/*",
-			"http://instagr.am/reel/*",
-			"https://instagr.am/reel/*"
-		],
-		"url": "https://api.instagram.com/oembed/",
-		"formats": ["json"],
-		builder: function (urlObj, obj) {
-			obj.icon = "https://www.instagram.com/favicon.ico";
-		}
-	}]
-};
-
-module.exports = [googlemaps, youtube, twitter, instagram];
+module.exports = [googlemaps, youtube, twitter];
 
